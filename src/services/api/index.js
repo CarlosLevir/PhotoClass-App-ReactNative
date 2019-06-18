0f36f6c1435d646bd77107adf5536fe7eca8007b
Carlos Levir <carloslevirfs@gmail.com>
2019-06-18 20:27:01 -0300

    Build of API Interceptor.

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,7 +1,18 @@
 import axios from 'axios';
+import { getToken } from '../auth/utils';
 
 const api = axios.create({
   baseURL: 'http://localhost:3333',
+});
+
+api.interceptors.request.use(async (config) => {
+  const token = await getToken();
+
+  if (token) {
+    // eslint-disable-next-line no-param-reassign
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default api;
