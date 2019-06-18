f7eafa742c9dc0d6ec3d64389bccd50ddc1665c1
Carlos Levir <carloslevirfs@gmail.com>
2019-06-18 20:27:34 -0300

    Auth Routes Divided.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,54 @@
+import React from 'react';
 import { createAppContainer, createStackNavigator } from 'react-navigation';
+import { TouchableOpacity, Platform } from 'react-native';
+
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import Signin from '../pages/Signin';
+import Signup from '../pages/Signup';
 import Main from '../pages/Main';
 import Subject from '../pages/Subject';
 
-const Routes = createAppContainer(
-  createStackNavigator({
-    Signin,
+const UnauthenticatedRoutes = createStackNavigator({
+  Signin,
+  Signup,
+});
+
+const AuthenticatedRoutes = createStackNavigator(
+  {
     Main,
     Subject,
-  }),
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      headerRight: (
+        <TouchableOpacity
+          style={{ marginRight: 15 }}
+          onPress={() => {
+            navigation.navigate('Signin');
+          }}
+        >
+          <Icon
+            name={Platform.OS === 'ios' ? 'ios-log-out' : 'md-log-out'}
+            size={24}
+            color="#000"
+          />
+        </TouchableOpacity>
+      ),
+    }),
+  },
+);
+
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      UnauthenticatedRoutes,
+      AuthenticatedRoutes,
+    },
+    {
+      headerMode: 'none',
+    },
+  ),
 );
 
 export default Routes;
