b3eaf651d67562900aa808ba1f96c313187e2636
Carlos Levir <carloslevirfs@gmail.com>
2019-06-21 07:33:06 -0300

    API Address from localhost to IP machine.

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from '../auth/utils';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'http://192.168.3.8:3333',
 });
 
 api.interceptors.request.use(async (config) => {
